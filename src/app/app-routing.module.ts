290b41a78969c2aa7f4de973c1c0d468ad79208d
Mateus Santos Fonseca <mateus.santos.fsc@gmail.com>
2020-01-04 21:11:42 -0300

    Finalizando estrutura, roteamento, header e footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { HabilidadeComponent } from './habilidade/habilidade.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'experiencias', component: ExperienciaComponent },
+  { path: 'historico', component: HistoricoComponent },
+  { path: 'habilidades', component: HabilidadeComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { HabilidadeComponent } from './habilidade/habilidade.component';
+import { HistoricoComponent } from './historico/historico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    NotFoundComponent,
+    HomeComponent,
+    ContatoComponent,
+    ExperienciaComponent,
+    HabilidadeComponent,
+    HistoricoComponent
   ],
   imports: [
     BrowserModule,
