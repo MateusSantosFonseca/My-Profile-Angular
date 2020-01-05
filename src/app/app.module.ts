04e5549336b9a598bce7764532cc902ba30682ae
Mateus Santos Fonseca <mateus.santos.fsc@gmail.com>
2020-01-05 05:50:49 -0300

    Adicionando conteudo pagina contato, adicionando estilos reusaveis novos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ContatoComponent } from './contato/contato.component';
 import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { HabilidadeComponent } from './habilidade/habilidade.component';
 import { HistoricoComponent } from './historico/historico.component';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +33,8 @@ import { HistoricoComponent } from './historico/historico.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
+
+
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  contadorCaracteres: string;
+  mensagemErro = '* O campo foi preenchido incorretamente';
+  emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  contactForm = new FormGroup({});
+
+  nome = new FormControl('', Validators.compose([
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(15)
+  ]));
+
+  sobrenome = new FormControl('', Validators.compose([
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(60)
+  ]));
+
+  email = new FormControl('', Validators.compose([
+    Validators.required,
+    Validators.pattern(this.emailRegex),
+    Validators.minLength(5)
+  ]));
+
+  telefone = new FormControl('', Validators.compose([
+    Validators.required,
+    Validators.minLength(8)
+  ]));
+
+
+  mensagem = new FormControl('', Validators.compose([
+    Validators.required,
+    Validators.maxLength(500)
+  ]));
+
+
+  constructor() {
+    this.contadorCaracteres = '';
+    this.contactForm.addControl('nome', this.nome);
+    this.contactForm.addControl('sobrenome', this.sobrenome);
+    this.contactForm.addControl('email', this.email);
+    this.contactForm.addControl('telefone', this.telefone);
+    this.contactForm.addControl('mensagem', this.mensagem);
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+    alert(this.nome.value + ', sua mensagem foi enviada com sucesso. Obrigado pelo contato!');
+    // Abrir popup ou modal, ver algum servico aque faz isso para alertar que foi concluir com sucesso
+    this.nome.reset();
+    this.sobrenome.reset();
+    this.email.reset();
+    this.telefone.reset();
+    this.mensagem.reset();
   }
 
 }
